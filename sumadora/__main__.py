80993b6a15bea1ea2631660385c3d5f67578595d
Santiago Quiñones-Cuenca <lsquinones@gmail.com>
2021-06-14 00:31:01 -0500

    Complete the code

--- sumadora/__main__.py
+++ sumadora/__main__.py
@@ -4,8 +4,12 @@ from sumadora.sumadora import sumar
 
 
 @click.command()
-@click.argument("numbers_string", nargs=-1, type=click.STRING)
-def main(numbers_string):
+@click.argument("numbers_string", type=click.STRING)
+def cli(numbers_string):
     numbers_string = numbers_string.split()
     numbers_int = list(map(lambda num: int(num), numbers_string))
-    result = sumar(numbers_int)
+    result = sumar(*numbers_int)
+    print(result)
+
+if __name__ == "__main__":
+    cli()
